5535548e82a6e7da608b5e1105640a6b8aae95ff
Enitoni <enitoni@gmail.com>
2020-11-30 01:51:57 +0100

    minor tweaks

--- src/modules/autoban/services/AutobanService.ts
+++ src/modules/autoban/services/AutobanService.ts
@@ -9,7 +9,7 @@ const storage = new JSONStorage<string[]>("autobans.json", [])
 export class AutobanService extends Service {
   public async serviceDidInitialize() {
     await storage.restore()
-    this.bot.client.on("message", m => this.handleMessage(m))
+    this.bot.client.on("message", (m) => this.handleMessage(m))
   }
 
   public get autobans() {
@@ -22,24 +22,25 @@ export class AutobanService extends Service {
   }
 
   public async remove(str: string) {
-    const newData = [...storage.data].filter(x => x !== str)
+    const newData = [...storage.data].filter((x) => x !== str)
     await storage.save(newData)
   }
 
   private async handleMessage(message: Message) {
     const { member, channel } = message
+    if (!member) return
 
-    if (member) {
-      const isSpam = this.autobans.some(x => message.content.includes(x))
-      if (!isSpam) return
+    const phrase = this.autobans.find((x) => message.content.includes(x))
+    if (!phrase) return
 
-      if (isAdmin(member)) {
-        channel.send("Poof! You're banned. ✨")
-      } else {
-        await message.delete()
-        await member.user.send({ embed: getBanNotifyEmbed() })
-        await member.ban()
-      }
+    if (isAdmin(member)) {
+      return channel.send("Poof! You're banned. ✨")
     }
+
+    await message.delete()
+    await member.user.send({ embed: getBanNotifyEmbed() })
+    await member.ban({
+      reason: `Banned phrase: ${phrase}`,
+    })
   }
 }
